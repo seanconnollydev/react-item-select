a3b2b8e88600afcb3bed42d72356ef6d10c111a6
Sean Connolly <sconnolly17@gmail.com>
2018-05-11 05:49:50 -0600

    The most basic example

--- demo/pages/index.js
+++ demo/pages/index.js
@@ -1,3 +1,31 @@
 import React from 'react';
+import withSelections from 'react-item-select';
 
-export default () => <div>Welcome to next.js!</div>;
+const items = [{
+  id: 1, name: 'One',
+}, {
+  id: 2, name: 'Two',
+}, {
+  id: 3, name: 'Three',
+}];
+
+const Index = (props) => {
+  const { handleSelect, isItemSelected } = props; // eslint-disable-line react/prop-types
+
+  return (
+    <div>
+      <ul>
+        {items.map(item => (
+          <li
+            key={item.id}
+            onClick={() => handleSelect(item.id)}
+            style={{ cursor: 'pointer', fontWeight: isItemSelected(item.id) ? 'bold' : 'normal' }}
+          >{item.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default withSelections(Index);
